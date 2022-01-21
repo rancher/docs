91245d5d3ae549bf0f35a3ddaa46b253e1a78516
Guilherme Macedo <guilherme.macedo@suse.com>
2022-01-21 11:35:08 +0100

    Add v2.6 hardening guides
    Signed-off-by: Guilherme Macedo <guilherme.macedo@suse.com>

--- scripts/converters/run_results_to_md.sh
+++ scripts/converters/run_results_to_md.sh
@@ -6,4 +6,4 @@ test_helpers=${2:?path to kube-bench test_helpers scripts is a required argument
 [ -f ${results} ] || (echo "file:'${results}' does not exist"; exit 1)
 [ -d ${test_helpers} ] || (echo "dir: '${test_helpers}' not a valid directory"; exit 1)
 
-docker run -v${results}:/source/results.json -v ${test_helpers}:/test_helpers -it --rm doc_converters:latest results_to_md
+docker run -v ${results}:/source/results.json -v ${test_helpers}:/test_helpers -it --rm doc_converters:latest results_to_md
